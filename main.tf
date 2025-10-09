788fa964c7d930d7dfeb1d5d32a8dbea5bbee578
davidlesicnik <david@lesicnik.eu>
2025-10-09 17:45:05 +0200

    added metallb

--- main.tf
+++ main.tf
@@ -12,23 +12,84 @@ terraform {
 }
 
 provider "kubernetes" {
-  # Forcing the path to the default kubeconfig file.
   config_path = "~/.kube/config"
 }
 
-# Configure the Helm provider to use the Kubernetes cluster
 provider "helm" {
   kubernetes = {
     config_path = "~/.kube/config"
   }
 }
 
+# MetalLB Namespace
+resource "kubernetes_namespace" "metallb_system" {
+  metadata {
+    name = "metallb-system"
+  }
+}
+
+# Install MetalLB using Helm
+resource "helm_release" "metallb" {
+  name       = "metallb"
+  repository = "https://metallb.github.io/metallb"
+  chart      = "metallb"
+  namespace  = kubernetes_namespace.metallb_system.metadata[0].name
+  version    = "0.14.5"
+
+  depends_on = [
+    kubernetes_namespace.metallb_system
+  ]
+}
+
+resource "kubernetes_manifest" "metallb_ipaddresspool" {
+  manifest = {
+    apiVersion = "metallb.io/v1beta1"
+    kind       = "IPAddressPool"
+    metadata = {
+      name      = "default-pool"
+      namespace = kubernetes_namespace.metallb_system.metadata[0].name
+    }
+    spec = {
+      addresses = [
+        "192.168.10.90-192.168.10.99"  # Adjust this range for your environment
+      ]
+    }
+  }
+
+  depends_on = [
+    helm_release.metallb
+  ]
+}
+
+# MetalLB L2 Advertisement
+resource "kubernetes_manifest" "metallb_l2advertisement" {
+  manifest = {
+    apiVersion = "metallb.io/v1beta1"
+    kind       = "L2Advertisement"
+    metadata = {
+      name      = "default"
+      namespace = kubernetes_namespace.metallb_system.metadata[0].name
+    }
+    spec = {
+      ipAddressPools = [
+        kubernetes_manifest.metallb_ipaddresspool.manifest.metadata.name
+      ]
+    }
+  }
+
+  depends_on = [
+    kubernetes_manifest.metallb_ipaddresspool
+  ]
+}
+
+# Ingress Nginx Namespace
 resource "kubernetes_namespace" "ingress_nginx" {
   metadata {
     name = "ingress-nginx"
   }
 }
 
+# Install Nginx Ingress Controller
 resource "helm_release" "nginx_ingress" {
   name       = "ingress-nginx"
   repository = "https://kubernetes.github.io/ingress-nginx"
@@ -37,6 +98,7 @@ resource "helm_release" "nginx_ingress" {
   version    = "4.10.1"
 
   depends_on = [
-    kubernetes_namespace.ingress_nginx
+    kubernetes_namespace.ingress_nginx,
+    kubernetes_manifest.metallb_l2advertisement
   ]
 }
