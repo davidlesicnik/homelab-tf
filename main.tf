a26be3a24fbb251d1815d63a3cb6b85199802683
davidlesicnik <david@lesicnik.eu>
2025-10-09 17:42:03 +0200

    added initial creation of ingress controller

--- main.tf
+++ main.tf
@@ -2,20 +2,41 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.0.0"
+      version = ">= 2.20.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.9.0"
     }
   }
 }
 
-# Configure the Kubernetes provider
-# It will automatically use the kubeconfig file from your home directory
 provider "kubernetes" {
-    config_path = "~/.kube/config"
+  # Forcing the path to the default kubeconfig file.
+  config_path = "~/.kube/config"
 }
 
-# Example: Create a new namespace to test the connection
-resource "kubernetes_namespace" "example" {
-  metadata {
-    name = "my-terraform-namespace"
+# Configure the Helm provider to use the Kubernetes cluster
+provider "helm" {
+  kubernetes = {
+    config_path = "~/.kube/config"
   }
+}
+
+resource "kubernetes_namespace" "ingress_nginx" {
+  metadata {
+    name = "ingress-nginx"
+  }
+}
+
+resource "helm_release" "nginx_ingress" {
+  name       = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  namespace  = kubernetes_namespace.ingress_nginx.metadata[0].name
+  version    = "4.10.1"
+
+  depends_on = [
+    kubernetes_namespace.ingress_nginx
+  ]
 }
