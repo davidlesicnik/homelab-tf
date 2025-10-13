aac0cabb66e72d93533f5d9b06852990b00c7a4c
David Lesicnik <david.lesicnik@dhimahi.com>
2025-10-13 14:12:40 +0200

    got vault working properly

--- vault/eso-policy.hcl
+++ vault/eso-policy.hcl
@@ -1,3 +1,9 @@
+# Allow reading secret data
 path "secret/data/*" {
   capabilities = ["read"]
+}
+
+# Allow listing and reading metadata
+path "secret/metadata/*" {
+  capabilities = ["list", "read"]
 }
--- vault/eso-policy.tf
+++ vault/eso-policy.tf
@@ -11,9 +11,10 @@ data "kubernetes_config_map_v1" "kube_root_ca" {
 resource "kubernetes_service_account_v1" "vault_reviewer" {
   metadata {
     name      = "vault-token-reviewer"
-    namespace = "default"
+    namespace = "vault"
   }
 }
+
 
 resource "kubernetes_secret_v1" "vault_reviewer_token" {
   metadata {
@@ -33,7 +34,7 @@ resource "vault_auth_backend" "kubernetes" {
 
 resource "vault_kubernetes_auth_backend_config" "k8s_config" {
   backend              = vault_auth_backend.kubernetes.path
-  kubernetes_host      = var.kubernetes_host
+  kubernetes_host      = "https://kubernetes.default.svc"
   # Use the automatically fetched CA certificate from the data source
   kubernetes_ca_cert   = data.kubernetes_config_map_v1.kube_root_ca.data["ca.crt"]
   token_reviewer_jwt   = kubernetes_secret_v1.vault_reviewer_token.data["token"]
@@ -50,13 +51,33 @@ resource "vault_policy" "eso_policy" {
 resource "vault_kubernetes_auth_backend_role" "eso_role" {
   backend                          = vault_auth_backend.kubernetes.path
   role_name                        = "external-secrets"
-  bound_service_account_names      = ["external-secrets"]
-  bound_service_account_namespaces = ["external-secrets"]
+  bound_service_account_names      = [kubernetes_service_account_v1.eso_sa.metadata[0].name]
+  bound_service_account_namespaces = [kubernetes_service_account_v1.eso_sa.metadata[0].namespace]
   token_policies                   = [vault_policy.eso_policy.name]
-  token_ttl                        = 3600 # 1 hour
+  token_ttl                        = 3600
+}
 
-  depends_on = [
-    vault_kubernetes_auth_backend_config.k8s_config,
-    vault_policy.eso_policy
-  ]
+resource "kubernetes_cluster_role_binding_v1" "vault_token_reviewer" {
+  metadata {
+    name = "vault-token-reviewer-binding"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "system:auth-delegator"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account_v1.vault_reviewer.metadata[0].name
+    namespace = kubernetes_service_account_v1.vault_reviewer.metadata[0].namespace
+  }
+}
+
+resource "kubernetes_service_account_v1" "eso_sa" {
+  metadata {
+    name      = "external-secrets"
+    namespace = "vault"
+  }
 }
