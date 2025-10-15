366141376e93a3b24981d60e245e0687b2c703f7
David Lesicnik <david.lesicnik@dhimahi.com>
2025-10-15 08:58:01 +0200

    set longhorn NS to be priv

--- k8s/longhorn.tf
+++ k8s/longhorn.tf
@@ -2,6 +2,11 @@
 resource "kubernetes_namespace" "longhorn_system" {
   metadata {
     name = "longhorn-system"
+    labels = {
+      "pod-security.kubernetes.io/enforce" = "privileged"
+      "pod-security.kubernetes.io/audit"   = "privileged"
+      "pod-security.kubernetes.io/warn"    = "privileged"
+    }
   }
 }
 
